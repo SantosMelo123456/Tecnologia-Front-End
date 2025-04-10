62a4166665fac4897068afdce17903cfdd19eec3
LindaMary <lindamariasmelo@gmail.com>
2025-04-09 21:17:50 -0300

    Add files via upload

---
+++ exercício 17, aula 09.py
@@ -0,0 +1,7 @@
+
+# Lista com nomes de amigos
+amigos =  [f'Sofia, Talita,Daniel, Julia']
+# Lista com nomes de amigos
+amigos= (input('Digite o nome de algum amigo:'))
+# Exibir uma saudação
+print ('Olá,como vai você?')
